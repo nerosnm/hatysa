bf6d562536a74b4a175f3ae3cd8750153edb99aa
Søren Mortensen <soren@neros.dev>
2020-09-23 00:38:49 +0100

    Working bot

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,85 @@
-fn main() -> eyre::Result<()> {
-    let params = [("long_url", "git.sr.ht/~nerosnm/sketchify-bot")];
-    let client = reqwest::Client::new();
-    let mut res = client
-        .post("http://verylegit.link/sketchify")
-        .form(&params)
-        .send()?;
+use std::env;
 
-    println!("{}", res.text()?);
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+    utils::MessageBuilder,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, context: Context, msg: Message) {
+        if msg.content == ",ping" {
+            let channel = match msg.channel_id.to_channel(&context).await {
+                Ok(channel) => channel,
+                Err(why) => {
+                    println!("Error getting channel: {:?}", why);
+
+                    return;
+                }
+            };
+
+            let response = MessageBuilder::new()
+                .push("User ")
+                .push_bold_safe(&msg.author.name)
+                .push(" used the 'ping' command in the ")
+                .mention(&channel)
+                .push(" channel")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+                println!("Error sending message: {:?}", why);
+            }
+        } else if msg.content.starts_with(",sketchify") {
+            let url_str = msg.content.strip_prefix(",sketchify").map(str::trim);
+
+            if let None = url_str {
+                println!("Error stripping prefix from URL.");
+                return;
+            }
+
+            let params = [("long_url", url_str.unwrap())];
+            let client = reqwest::Client::new();
+            let res = client
+                .post("http://verylegit.link/sketchify")
+                .form(&params)
+                .send();
+
+            let response = match res.and_then(|mut res| res.text()) {
+                Err(err) => MessageBuilder::new()
+                    .push_bold("Error")
+                    .push(" sketchifying link: ")
+                    .push_mono_safe(err)
+                    .build(),
+                Ok(res) => MessageBuilder::new().push(res).build(),
+            };
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    // Configure the client with your Discord bot token in the environment.
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 
     Ok(())
 }
