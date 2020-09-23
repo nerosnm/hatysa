6d3dbea7808d16928e26a7bb1fea07f483d5ba0e
Søren Mortensen <soren@neros.dev>
2020-09-23 22:57:20 +0100

    Add URL parsing in ,sketchify and remove ,echo

--- src/main.rs
+++ src/main.rs
@@ -35,45 +35,43 @@ impl EventHandler for Handler {
                 println!("Error sending message: {:?}", why);
             }
         } else if msg.content.starts_with(",sketchify") {
-            let url_str = msg.content.strip_prefix(",sketchify").map(str::trim);
-
-            if let None = url_str {
-                println!("Error stripping prefix from URL.");
-                return;
-            }
-
-            let params = [("long_url", url_str.unwrap())];
-            let client = reqwest::Client::new();
-            let res = client
-                .post("http://verylegit.link/sketchify")
-                .form(&params)
-                .send();
-
-            let response = match res.and_then(|mut res| res.text()) {
-                Err(err) => MessageBuilder::new()
-                    .push_bold("Error")
-                    .push(" sketchifying link: ")
-                    .push_mono_safe(err)
-                    .build(),
-                Ok(res) => MessageBuilder::new().push_safe(res).build(),
-            };
-
-            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
-                println!("Error sending message: {:?}", why);
-            }
-        } else if msg.content.starts_with(",echo") {
-            let url_res = msg
+            let sketchified_url = msg
                 .content
-                .strip_prefix(",echo")
+                .strip_prefix(",sketchify")
                 .map(str::trim)
-                .ok_or("Error extracting URL from message".to_string())
+                .ok_or("extracting URL from message".to_string())
+                .and_then(|url_str| Url::parse(url_str).map_err(|e| format!("parsing URL: {}", e)))
+                .and_then(|url| {
+                    let api_params = [("long_url", url.to_string())];
+                    let client = reqwest::Client::new();
+                    client
+                        .post("http://verylegit.link/sketchify")
+                        .form(&api_params)
+                        .send()
+                        .map_err(|err| format!("in request: {}", err))
+                })
+                .and_then(|mut res| {
+                    if res.status().is_success() {
+                        res.text()
+                            .map_err(|err| format!("parsing API response: {}", err))
+                    } else {
+                        Err(format!("in API, response code: {}", res.status()))
+                    }
+                })
+                .map(|url_str| {
+                    if !url_str.starts_with("http") {
+                        format!("http://{}", url_str)
+                    } else {
+                        url_str
+                    }
+                })
                 .and_then(|url_str| {
-                    Url::parse(url_str).map_err(|e| format!("Unable to parse URL: {}", e))
+                    Url::parse(&*url_str).map_err(|err| format!("parsing returned URL: {}", err))
                 });
 
-            let response = match url_res {
+            let response = match sketchified_url {
                 Ok(url) => MessageBuilder::new().push(url).build(),
-                Err(err) => MessageBuilder::new().push_bold("Error: ").push(err).build(),
+                Err(err) => MessageBuilder::new().push_bold("Error ").push(err).build(),
             };
 
             if let Err(why) = msg.channel_id.say(&context.http, &response).await {
