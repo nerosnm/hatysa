663f126a96c79dedc3e8cc404c7b4b8b96f83a7c
Søren Mortensen <soren@neros.dev>
2020-09-28 23:31:24 +0100

    Add the ,react command

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ use serenity::{
         macros::{command, group},
         Args, CommandResult, StandardFramework,
     },
-    model::{channel::Message, gateway::Activity, gateway::Ready},
+    model::{
+        channel::{Message, ReactionType},
+        gateway::Activity,
+        gateway::Ready,
+    },
     prelude::*,
     utils::MessageBuilder,
 };
 use url::Url;
 
+use std::collections::HashMap;
 use std::env;
 
 #[command]
@@ -69,8 +74,92 @@ async fn sketchify(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     Ok(())
 }
 
+#[command]
+#[num_args(1)]
+async fn react(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let input_str = args.parse::<String>()?;
+
+    // An input string is only valid if it is entirely composed of alphanumeric
+    // characters, and if each one only appears once.
+    let non_alphanum = input_str.contains(|c: char| !c.is_alphanumeric());
+    let valid = !non_alphanum
+        && input_str
+            .chars()
+            .fold(HashMap::new(), |mut acc, next| {
+                *acc.entry(next.to_ascii_uppercase()).or_insert(0) += 1;
+                acc
+            })
+            .values()
+            .all(|&v| v == 1);
+
+    if valid {
+        if let Some(prev) = msg
+            .channel_id
+            .messages(&ctx.http, |retriever| retriever.before(msg.id).limit(1))
+            .await?
+            .first()
+        {
+            for c in to_reactions(&input_str) {
+                prev.react(&ctx.http, c).await?;
+            }
+        }
+
+        msg.delete(&ctx.http).await?;
+    } else if non_alphanum {
+        let response = MessageBuilder::new()
+            .push("String ")
+            .push_bold_safe(input_str.to_ascii_uppercase())
+            .push(" contains non-alphanumeric characters!")
+            .build();
+
+        msg.channel_id.say(&ctx.http, &response).await?;
+    } else {
+        let response = MessageBuilder::new()
+            .push("String ")
+            .push_bold_safe(input_str.to_ascii_uppercase())
+            .push(" contains repeated characters!")
+            .build();
+
+        msg.channel_id.say(&ctx.http, &response).await?;
+    }
+
+    Ok(())
+}
+
+const VARIATION_SELECTOR_16: u32 = 0xfe0f;
+const COMBINING_ENCLOSING_KEYCAP: u32 = 0x20e3;
+
+/// Convert a string to a sequence of reactions representing its characters,
+/// using regional indicators for alphabetic characters and keycap sequences for
+/// numerals. Any non-ascii-alphanumeric characters are simply left
+/// as-is in the output string.
+fn to_reactions(input: &str) -> Vec<ReactionType> {
+    input
+        .chars()
+        .map(|c| match c {
+            'A'..='Z' => std::char::from_u32(c as u32 + 0x1f1a5)
+                .unwrap_or(c)
+                .to_string(),
+            'a'..='z' => std::char::from_u32(c as u32 + 0x1f185)
+                .unwrap_or(c)
+                .to_string(),
+            '0'..='9' => {
+                // Create a keycap sequence by adding U+20E3 COMBINING ENCLOSING
+                // KEYCAP after the numeral.
+                let mut num = String::new();
+                num.push(c);
+                num.push(unsafe { std::char::from_u32_unchecked(VARIATION_SELECTOR_16) });
+                num.push(unsafe { std::char::from_u32_unchecked(COMBINING_ENCLOSING_KEYCAP) });
+                num
+            }
+            _ => c.to_string(),
+        })
+        .map(ReactionType::Unicode)
+        .collect()
+}
+
 #[group]
-#[commands(ping, sketchify)]
+#[commands(ping, sketchify, react)]
 struct General;
 
 struct Handler;
@@ -80,8 +169,7 @@ impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
-        ctx.set_activity(Activity::playing(",sketchify <url>"))
-            .await;
+        ctx.set_activity(Activity::playing(",react")).await;
     }
 }
 
